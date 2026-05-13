ecd0c356543e7805a2da17fbf8c8e20513e96ee0
joung-ai <yopkj12@gmail.com>
2026-05-13 11:37:31 +0800

    condos and houses needed to be filled with properties only, and the details page needed to be filled with the details of the properties. The index page needed to be filled with the properties as well, and the style.css needed to be edited to make the website look better. The script.js needed to be edited to make the website work properly.

--- script.js
+++ script.js
@@ -28,103 +28,17 @@ document.addEventListener("DOMContentLoaded", function () {
 
 // NAVBAR SCROLL EFFECT
 
-
 const nav = document.querySelector("nav");
 
-window.addEventListener("scroll", function () {
+if(document.body.classList.contains("home-page")){
 
-  nav.classList.toggle("slidedown", window.scrollY > 50);
+  window.addEventListener("scroll", function () {
 
-});
+    nav.classList.toggle(
+      "slidedown",
+      window.scrollY > 50
+    );
 
+  });
 
-// HOUSE A SLIDER//
-
-
-const propertyImages = [
-
-  "images/House_A_1.jpg",
-  "images/House_A_2.jpg",
-  "images/House_A_3.jpg",
-  "images/House_A_4.jpg",
-  "images/House_A_5.jpg",
-  "images/House_A_6.jpg",
-  "images/House_A_7.jpg",
-  "images/House_A_8.jpg",
-  "images/House_A_9.jpg",
-  "images/House_A_10.jpg"
-
-];
-
-let current = 0;
-
-const slider = document.getElementById("slider");
-
-
-function next() {
-
-  current++;
-
-  if (current >= propertyImages.length) {
-    current = 0;
-  }
-
-  slider.src = propertyImages[current];
-
-}
-
-
-function prev() {
-
-  current--;
-
-  if (current < 0) {
-    current = propertyImages.length - 1;
-  }
-
-  slider.src = propertyImages[current];
-
-}
-
-
-// HOUSE B SLIDER//
-
-
-const propertyImages = [
-
-  "images/House_B_1.jpg",
-  "images/House_B_2.jpg",
-  "images/House_B_3.jpg",
-  "images/House_B_4.jpg",
-  "images/House_B_5.jpg",
-  "images/House_B_6.jpg",
-  "images/House_B_7.jpg",
-  "images/House_B_8.jpg",
-  "images/House_B_9.jpg",
-  "images/House_B_10.jpg"
-
-];
-
-let current = 0;
-
-const slider = document.getElementById("slider");
-
-function next() {
-
-  current++;
-
-  if (current >= propertyImages.length) {
-    current = 0;
-  }
-  slider.src = propertyImages[current];
-}
-
-function prev() {
-
-  current--;
-
-  if (current < 0) {
-    current = propertyImages.length - 1;
-  }
-  slider.src = propertyImages[current];
 }
